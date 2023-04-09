c7995c98dfd16269260140887518862fb58cc814
Obadias Pelembe Junior <jr.pelembeobadias@gmail.com>
2023-04-09 16:45:39 +0000

    Merged in feat/tf-backend (pull request #2)
    feat: implement initial infrastructure configurations
    
    * chore: run apply without tfstate
    
    * chore: use tfstate instead
    
    * fix: use tf-state instead
    
    * fix: use tfstate instead
    
    * fix: point tfstate to right path
    
    * chore: push to linsync
    
    * chore: sync on terraform apply
    
    * chore: remove unnecessary commands
    
    * chore: add more stages and pipeline condition
    
    * chore: add more condition

--- src/main.tf
+++ src/main.tf
@@ -1,4 +1,18 @@
 terraform {
+
+  # backend "s3" {
+  #   endpoint                    = "state-lock.us-southeast-1.linodeobjects.com" 
+  #   skip_credentials_validation = true
+  #   bucket                      = "state-lock"
+  #   key                         = "tf-ci-state/custom-pipeline-001.json" 
+  #   region = "us-southeast-1"
+    
+  # }
+
+  # backend "http" {
+  #   address = "https://api.linode.com/v4/object-storage/keys/custom-pipeline-001"
+  #   update_method = "PUT"
+  # }
   required_providers {
     linode = {
       source = "linode/linode"
